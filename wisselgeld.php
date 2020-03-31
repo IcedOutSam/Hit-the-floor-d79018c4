def6347a8d5bccbe10c67adefd9233ae61c92072
Sam Ahmed <2102926@talnet.nl>
2020-03-31 14:20:20 +0200

    Uitwerking van de exercise

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,17 +1,23 @@
 <?php
-$geld = array('10', '5', '2', '1');
-$aantal = $argv[1];
+$geld      = array(
+    '50',
+    '20',
+    '10',
+    '5',
+    '2',
+    '1'
+);
+$aantal    = $argv[1];
 $restanten = $aantal;
 foreach ($geld as $value) {
-  if ($aantal >0) {
-    $totaal = floor($restanten / $value);
-    echo "U krijgt $totaal x €$value " . PHP_EOL;
-      $aantal = $restanten;
-      $restanten = floor($restanten % $value);
-  }
-  else {
-    echo "Geen wisselgeld, batti";
-  }
+    if ($aantal > 0) {
+        $totaal = floor($restanten / $value);
+        if ($totaal > 0) {
+            echo "U krijgt $totaal x €$value " . PHP_EOL;
+            $aantal    = $restanten;
+            $restanten = floor($restanten % $value);
+        }
+    }
 }
 
- ?>
+?>
